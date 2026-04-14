1c1032f8624d19be48b5695ab1216ccace419cb2
leonardolima <leo-lima-13@hotmail.com>
2026-04-13 22:18:23 -0300

    Add OpenTelemetry with Application Insights to Worker

--- OrderProcessing.Worker/Program.cs
+++ OrderProcessing.Worker/Program.cs
@@ -1,3 +1,7 @@
+using Azure.Monitor.OpenTelemetry.Exporter;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using OrderProcessing.Infrastructure.Module;
 using OrderProcessing.Worker;
 using OrderProcessing.Worker.Module;
@@ -15,6 +19,15 @@ try
         .ReadFrom.Configuration(builder.Configuration)
         .WriteTo.Console(outputTemplate:
             "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}"));
+
+    var appInsightsConnection = builder.Configuration["ApplicationInsights:ConnectionString"];
+    if (!string.IsNullOrEmpty(appInsightsConnection))
+    {
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(r => r.AddService("OrderProcessing.Worker"))
+            .WithTracing(t => t.AddAzureMonitorTraceExporter(o =>
+                o.ConnectionString = appInsightsConnection));
+    }
 
     builder.Services.AddInfrastructure(builder.Configuration);
     builder.Services.AddWorker();
